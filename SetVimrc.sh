d7ced1535c60f95fb2fbf701e8ea29c4d1b02939
Utpal Bora <cs14mtech11017@iith.ac.in>
2018-10-16 10:04:25 +0530

    Python headers missing

--- SetVimrc.sh
+++ SetVimrc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install -fmy vim git build-essential gcc g++ cmake autoconf libtool automake make m4 flex bison gawk python python3 clang-format clang llvm 
+sudo apt-get install -fmy vim git build-essential gcc g++ cmake autoconf libtool automake make m4 flex bison gawk python python3 python3-dev clang-format clang llvm 
 
 if ! test -d ~/.vim/bundle/Vundle.vim; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -8,4 +8,5 @@ fi
 cp .vimrc ~/.vimrc
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe && python3 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
-cp ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
+#cp ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
+cp ~/.vim/bundle/YouCompleteMe/third_party/ycmd/.ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
