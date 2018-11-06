7594af5e1ed7930c845dd7369d9c3b7c2aca35d7
Utpal Bora <cs14mtech11017@iith.ac.in>
2018-11-06 15:22:59 +0530

    Added zsh shell support, relative numbering in vim and renamed vimrc script

---
+++ zsh.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+sudo apt-get install -fmy zsh fonts-powerline git curl wget unzip dconf-cli
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s `which zsh`
+if ! test -d ~/.solarized; then
+  git clone git://github.com/sigurdga/gnome-terminal-colors-solarized.git ~/.solarized
+fi
+~/.solarized/install.sh
+wget https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.ansi-dark -O ~/.solarized/dircolors.ansi-dark
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+mkdir -p ~/.fonts
+mkdir -p ~/.config/fontconfig/conf.d
+mv PowerlineSymbols.otf ~/.fonts/
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+wget https://github.com/madmalik/mononoki/releases/download/1.2/mononoki.zip -O mononoki.zip
+mkdir -p ~/.fonts/mononoki
+unzip -u mononoki.zip -d ~/.fonts/mononoki/
+wget https://fonts.google.com/download\?family\=Source%20Code%20Pro -O Source_Code_Pro.zip
+mkdir -p ~/.fonts/Source_Code_Pro
+unzip -u Source_Code_Pro.zip -d ~/.fonts/Source_Code_Pro
+fc-cache -vf ~/.fonts/
+sudo apt-get install -fmy autojump python-pygments
+if ! test -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+if ! test -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+count=$(grep -c "DEFAULT_USER=\`whoami\`" ~/.zshrc)
+if [ $count -eq 0 ]; then
+  echo "DEFAULT_USER=\`whoami\`" >> ~/.zshrc
+fi
+count1=$(grep -c "eval \`dircolors ~/.solarized/dircolors.ansi-dark\`" ~/.zshrc)
+if [ $count1 -eq 0 ]; then
+  echo "eval \`dircolors ~/.solarized/dircolors.ansi-dark\`" >> ~/.zshrc
+fi
+
+sed -i "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"agnoster\"/" ~/.zshrc
